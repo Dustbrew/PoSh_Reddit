3d7e1887e8dead6bcf72bec657baa7aed682e62e
Dave <dave@garnar.com>
2013-08-08 17:52:24 +0100

    Added error handling to Submit-Post and changed Resolve-Captchs to prompt for an answer with windows forms

--- PoShRedditBot.psm1
+++ PoShRedditBot.psm1
@@ -127,6 +127,7 @@ Submit-Post -PostType link -SubReddit "PowerShell" -PostLink "http://www.powersh
 
 .OUTPUTS
 
+
 .NOTES
 Created By: Dave Garnar/Davotronic5000
 
@@ -171,6 +172,14 @@ FUNCTION Submit-Post
         [BOOL]$FailOnCaptcha = $False
         )
 
+#add a default footer to posts made by the bot
+    $PostBody = @"
+$PostBody
+---
+This is an automated post submitted by PoShBot a reddit bot written in PowerShell by /u/Davotronic5000.
+My Source code is avaialable here: [PoShBot Git](https://github.com/davotronic5000/PoSh_Reddit_Bot)
+"@
+
     $params = @{
     "api_type" = $Global:ApiType
     "kind" = $PostType
@@ -184,33 +193,20 @@ FUNCTION Submit-Post
     "url" = $PostLink
     }
 
-#add a default footer to posts made by the bot
-    $PostBody = @"
-$PostBody
----
-This is an automated post submitted by PoShBot a reddit bot written in PowerShell by /u/Davotronic5000.
-"@
-
-
-
     IF ($psCmdlet.ShouldProcess("## object ##", "## message ##"))
         {
         TRY
             {
-            $Submit = Invoke-WebRequest -uri "$Global:BaseUrl/api/submit" -Method Post -Body $Params -WebSession $GLOBAL:Session -UserAgent $Global:UserAgent | ConvertFrom-Json
+            $Global:Submit = Invoke-WebRequest -uri "$Global:BaseUrl/api/submit" -Method Post -Body $Params -WebSession $GLOBAL:Session -UserAgent $Global:UserAgent | ConvertFrom-Json
             }
         CATCH
             {
             Write-Error -RecommendedAction Stop -Message "Failed to submit the post to Reddit" -Exception $_.Exception.Message
             }
 
-        IF ($Submit.json.Data)
-            {
-            Write-Verbose "Post successfully submitted to reddit"
-            Write-Output $Submit
-            }
+
         # If post failed because of captcha, check if fail on captcha is set to false
-        ELSEIF ($Submit.json.errors -like "*BAD_CAPTCHA*")
+        IF ($Submit.json.errors -like "*BAD_CAPTCHA*")
             {
             IF (!$FailOnCaptcha)
                 {
@@ -220,13 +216,28 @@ This is an automated post submitted by PoShBot a reddit bot written in PowerShel
                 "captcha" = $Captcha.Answer
                 "iden" = $Captcha.Iden
                 }
-                $SubmitWithCaptcha = Invoke-WebRequest -uri "$Global:BaseUrl/api/submit" -Method Post -Body $Params -WebSession $GLOBAL:Session -UserAgent $Global:UserAgent | ConvertFrom-Json
-                Write-Output $SubmitWithCaptcha
+                $Submit = Invoke-WebRequest -uri "$Global:BaseUrl/api/submit" -Method Post -Body $Params -WebSession $GLOBAL:Session -UserAgent $Global:UserAgent | ConvertFrom-Json
+                Write-Output $Submit
                 }
             }
-        }
 
+        
+        IF ($Submit.json.Data)
+            {
+            Write-Verbose "Post successfully submitted to reddit"
+            Write-Output $Submit
+            }
+        ELSEIF ($Submit.json.errors -like "*QUOTA_FILLED*")
+            {
+            Write-Error -RecommendedAction Stop -Message "$($Submit.json.errors)"
+            }
+        ELSEIF ($Submit.json.errors -like "*RATELIMIT*")
+            {
+            Write-Error -RecommendedAction Stop -Message "$($Submit.json.errors)"
+            }
+        }
     }
+
 #endregion
 
 #region Resolve-Captcha
@@ -265,24 +276,159 @@ FUNCTION Resolve-Captcha
         [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
         [String]$CaptchaIden
         )
-    Write-Verbose "Create Captch URL and copy to clipboard"
-    $Captcha = "$Global:BaseUrl/captcha/$CaptchaIden"
-    $Captcha | CLip
 
-    IF ($psCmdlet.ShouldProcess($CaptchaIden, "Prompting for soution of Captcha"))
+    Begin
         {
-        #Prompt user to answer captcha
-        $CaptchaAnswer = Read-Host "Please go to $Captcha in a browser and enter the captcha text here (the link has already been placed in your clipboard):"
+        #region create captcha form
+        #Import Windows formas assemblies
+        [reflection.assembly]::loadwithpartialname("System.Windows.Forms") | Out-Null
+        [reflection.assembly]::loadwithpartialname("System.Drawing") | Out-Null
 
-        Write-Verbose "Outputting Captcha data"
-        $output = [ORDERED] @{
-        "Iden" = $CaptchaIden
-        "Answer" = $CaptchaAnswer
+        #Generate forms objects
+        $frmCaptcha = New-Object System.Windows.Forms.Form
+        $btnNewCaptcha = New-Object System.Windows.Forms.Button
+        $btnSubmit = New-Object System.Windows.Forms.Button
+        $txtCaptcha = New-Object System.Windows.Forms.TextBox
+        $imgCaptcha = New-Object System.Windows.Forms.PictureBox
+        $InitialFormWindowState = New-Object System.Windows.Forms.FormWindowState
+
+        $System_Drawing_Size = New-Object System.Drawing.Size
+        $System_Drawing_Size.Height = 159
+        $System_Drawing_Size.Width = 264
+        $frmCaptcha.ClientSize = $System_Drawing_Size
+        $frmCaptcha.DataBindings.DefaultDataSourceUpdateMode = 0
+        $frmCaptcha.Name = "frmCaptcha"
+        $frmCaptcha.Text = "Catcha Response"
+
+
+        $btnNewCaptcha.DataBindings.DefaultDataSourceUpdateMode = 0
+
+        $System_Drawing_Point = New-Object System.Drawing.Point
+        $System_Drawing_Point.X = 144
+        $System_Drawing_Point.Y = 94
+        $btnNewCaptcha.Location = $System_Drawing_Point
+        $btnNewCaptcha.Name = "btnNewCaptcha"
+        $System_Drawing_Size = New-Object System.Drawing.Size
+        $System_Drawing_Size.Height = 50
+        $System_Drawing_Size.Width = 108
+        $btnNewCaptcha.Size = $System_Drawing_Size
+        $btnNewCaptcha.TabIndex = 3
+        $btnNewCaptcha.Text = "New Captcha"
+        $btnNewCaptcha.UseVisualStyleBackColor = $True
+        $btnNewCaptcha.add_Click($btnNewCaptcha_OnClick)
+
+        $frmCaptcha.Controls.Add($btnNewCaptcha)
+
+
+        $btnSubmit.DataBindings.DefaultDataSourceUpdateMode = 0
+
+        $System_Drawing_Point = New-Object System.Drawing.Point
+        $System_Drawing_Point.X = 12
+        $System_Drawing_Point.Y = 94
+        $btnSubmit.Location = $System_Drawing_Point
+        $btnSubmit.Name = "btnSubmit"
+        $System_Drawing_Size = New-Object System.Drawing.Size
+        $System_Drawing_Size.Height = 50
+        $System_Drawing_Size.Width = 109
+        $btnSubmit.Size = $System_Drawing_Size
+        $btnSubmit.TabIndex = 2
+        $btnSubmit.Text = "Submit"
+        $btnSubmit.UseVisualStyleBackColor = $True
+
+        $frmCaptcha.Controls.Add($btnSubmit)
+
+        $txtCaptcha.DataBindings.DefaultDataSourceUpdateMode = 0
+        $System_Drawing_Point = New-Object System.Drawing.Point
+        $System_Drawing_Point.X = 46
+        $System_Drawing_Point.Y = 68
+        $txtCaptcha.Location = $System_Drawing_Point
+        $txtCaptcha.Name = "txtCaptcha"
+        $System_Drawing_Size = New-Object System.Drawing.Size
+        $System_Drawing_Size.Height = 20
+        $System_Drawing_Size.Width = 169
+        $txtCaptcha.Size = $System_Drawing_Size
+        $txtCaptcha.TabIndex = 1
+
+        $frmCaptcha.Controls.Add($txtCaptcha)
+
+        $imgCaptcha.AccessibleDescription = "CaptchaImage"
+        $imgCaptcha.AccessibleName = "CaptchaImage"
+        $imgCaptcha.DataBindings.DefaultDataSourceUpdateMode = 0
+
+
+
+        $System_Drawing_Point = New-Object System.Drawing.Point
+        $System_Drawing_Point.X = 72
+        $System_Drawing_Point.Y = 12
+        $imgCaptcha.Location = $System_Drawing_Point
+        $imgCaptcha.Name = "imgCaptcha"
+        $System_Drawing_Size = New-Object System.Drawing.Size
+        $System_Drawing_Size.Height = 50
+        $System_Drawing_Size.Width = 120
+        $imgCaptcha.Size = $System_Drawing_Size
+        $imgCaptcha.TabIndex = 0
+        $imgCaptcha.TabStop = $False
+
+        $frmCaptcha.Controls.Add($imgCaptcha)
+
+        #endregion
         }
-    }
-    New-Object PSObject -Property $Output
+
+    Process
+        {
+        Write-Verbose "Getting Captcha image to present to user"
+        $Captcha = Invoke-WebRequest -uri "$Global:BaseURL/captcha/$CaptchaIden"
+        $imgCaptcha.Image = [System.Drawing.Image]::FromStream($Captcha.RawContentStream)
+
+        #declare script block to run when submit button is clicked.
+        $btnSubmit_OnClick= 
+            {
+            #set variable to show answer has been submitted
+            $Submit = $true
+            #close form window
+            $frmCaptcha.Close()
+            }
+        #assign script block to submit button
+        $btnSubmit.add_Click($btnSubmit_OnClick)
+
+        #declare script block to run when new captcha button is clicked.
+        $btnNewCaptcha_OnClick= 
+        {
+        #get new captcha image iden
+        $params = @{
+        "api_type" = $Global:ApiType
+        }
+        #request new captcha iden
+        $CaptchaIden = Invoke-WebRequest -uri "$Global:BaseUrl/api/new_captcha" -Method Post -Body $Params -WebSession $GLOBAL:Session -UserAgent $Global:UserAgent | ConvertFrom-Json
+        #get captch image
+        $Captcha = Invoke-WebRequest -uri "$Global:BaseURL/captcha/$($CaptchaIden.json.data.iden)"
+        #present image to user
+        $imgCaptcha.Image = [System.Drawing.Image]::FromStream($Captcha.RawContentStream)
+        }
+        $btnNewCaptcha.add_Click($btnNewCaptcha_OnClick)
+
+        #Load form
+        $InitialFormWindowState = $frmCaptcha.WindowState
+        $frmCaptcha.add_Load($OnLoadForm_StateCorrection)
+        $frmCaptcha.ShowDialog() | Out-Null
+        }
+    
+    End
+        {
+        #If the captcha answer has been submitted output it from the function
+        IF ($Submit)
+            {
+            $output = [ORDERED] @{
+                "Iden" = $CaptchaIden
+                "Answer" = $txtCaptcha.Text.ToUpper()
+                }
+            New-Object PSObject -Property $Output
+            }
+
+        }
 
     }
+
 #endregion
 
 #region Help-Template
