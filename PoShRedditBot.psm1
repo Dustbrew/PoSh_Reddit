31284dea6f59d2935e8fb74dc9defb18b3d518f6
Dave <dave@garnar.com>
2013-08-09 10:33:39 +0100

    Started adding API error handling

--- PoShRedditBot.psm1
+++ PoShRedditBot.psm1
@@ -434,7 +434,209 @@ FUNCTION Resolve-Captcha
 #region Find-Errors
 FUNCTION Find-Errors
     {
+    [CmdletBinding()]
+    PARAM
+        (
+        $input
+        )
+    SWITCH -Regex ($input.json.errors)
+        {
+        "USER_REQUIRED" {}
 
+        "HTTPS_REQUIRED" {}
+
+        "VERIFIED_USER_REQUIRED" {}
+
+        "NO_URL" {}
+
+        "BAD_URL" {}
+
+        "BAD_CAPTCHA" {}
+
+        "BAD_USERNAME" {}
+
+        "USERNAME_TAKEN" {}
+
+        "USERNAME_TAKEN_DEL" {}
+
+        "USER_BLOCKED" {}
+
+        "NO_THING_ID" {}
+
+        "TOO_MANY_THINGS_ID" {}
+
+        "NOT_AUTHOR" {}
+
+        "NOT_USER" {}
+
+        "LOGGED_IN" {}
+
+        "DELETED_LINK" {}
+
+        "DELETED_COMMENT" {}
+
+        "DELETED_THING" {}
+
+        "BAD_PAASWORD" {}
+
+        "WRONG_PASSWORD" {}
+
+        "BAD_PASSWORD_MATCH" {}
+
+        "NO_NAME" {}
+
+        "NO_EMAIL" {}
+
+        "NO_EMAIL_FOR_USER" {}
+
+        "NO_TO_ADDRESS" {}
+
+        "NO_SUBJECT" {}
+
+        "USER_DOESENT_EXIST" {}
+
+        "NO_USER" {}
+
+        "INVALID_PREF" {}
+
+        "BAD_NUMBER" {}
+
+        "BAD_STRING" {}
+
+        "BAD_BID" {}
+
+        "ALREADY_SUB" {}
+
+        "SUBREDDIT_EXISTS" {}
+
+        "SUBREDDIT_NOTALLOWED" {}
+
+        "SUBREDDIT_REQUIRED" {}
+
+        "BAD_SR_NAME" {}
+
+        "RATELIMIT" {}
+
+        "QUOTA_FILLED" {}
+
+        "SUBREDDIT_RATELIMIT" {}
+
+        "EXPIRED" {}
+
+        "DRACONIAN" {}
+
+        "BANNED_IP" {}
+
+        "BAD_CNAME" {}
+
+        "USED_CNAME" {}
+
+        "INVALID_OPTION" {}
+
+        "CHEATER" {}
+
+        "BAD_EMAILS" {}
+
+        "NO_EMAILS" {}
+
+        "TOO_MANY_EMAILS" {}
+
+        "OVERSOLD" {}
+
+        "BAD_DATE" {}
+
+        "BAD_DATE_RANGE" {}
+
+        "DATE_RANGE_TOO_LARGE" {}
+
+        "BAD_FUTURE_DATE" {}
+
+        "BAD_PAST_DATE" {}
+
+        "BAD_ADDRESS" {}
+
+        "BAD_CARD" {}
+
+        "TOO_LONG" {}
+
+        "NO_TEXT" {}
+
+        "INVALID_CODE" {}
+
+        "CLAIMED_CODE" {}
+
+        "NO_SELFS" {}
+
+        "NO_LINKS" {}
+
+        "TOO_OLD" {}
+
+        "BAD_CSS_NAME" {}
+
+        "BAD_CSS" {}
+
+        "BAD_REVISION" {}
+
+        "TOO_MUCH_FLAIR_CSS" {}
+
+        "BAD_FLAIR_TARGET" {}
+
+        "CONFIRM" {}
+       
+        "CONFLICT" {}
+
+        "NO_API" {}
+
+        "DOMAIN_BANNED" {}
+
+        "NO_OTP_SECRET" {}
+
+        "NOT_SUPPORTED" {}
+
+        "BAD_IMAGE" {}
+
+        "DEVELOPER_ALREADY_ADDED" {}
+
+        "TOO_MANY_DEVELOPERS" {}
+
+        "BAD_HASH" {}
+
+        "ALREADY_MODERATOR" {}
+
+        "NO_INVITE_FOUND" {}
+
+        "BID_LIVE" {}
+
+        "TOO_MANY_CAMPAIGNS" {}
+
+        "BAD_JASONP_CALLBACK" {}
+
+        "INVALID_PERMISSION_TYPE" {}
+
+        "INVALID_PERMISSIONS" {}
+
+        "BAD_MULTI_PATH" {}
+
+        "BAD_MULTI_NAME" {}
+
+        "MULTI_NOT_FOUND" {}
+
+        "MULTI_EXISTS" {}
+
+        "MULTI_CANNOT_EDIT" {}
+
+        "MULTI_TOO_MANY_SUBREDDITS" {}
+
+        "MULTI_SPECIAL_SUBREDDIT" {}
+
+        "JSON_PARSE_ERROR" {}
+
+        "JSON_INVALID" {}
+
+        "JOSN_MISSING_KEY" {}
+
+        Default {}
+        }
     }
 #endregion
 
